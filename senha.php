c63e3b478eee9321010818342d24b5a900abcc3d
lucaskumagae <lucas.kumagae@gmail.com>
2024-11-07 18:05:11 -0300

    tela de recuperar senha

---
+++ senha.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Senha</title>
+    <link rel="stylesheet" href="senha.css">
+    <style>
+        body {
+            background-image: url(img/background.jpeg);
+            background-size: 100% auto;
+            background-position: center;
+            background-repeat: no-repeat;
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+    <div class="logo">
+        <img src="img/logo.jpeg" alt="Acknowledge Building">
+    </div>
+    <div class="container">
+        <div class="form-container">
+            <h2>Recuperar Senha</h2>
+            <p>Informe o endereço que deseja recuperar e enviaremos um email com as instruções de recuperação</p>
+            <form action="#" method="post">
+                <input type="email" placeholder="Email" required>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>

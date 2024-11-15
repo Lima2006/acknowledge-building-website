8ad42d78de9d9a3c89869e83d5c5254587154f6b
Sérgio Pizzatto Tulio <sergiotulio1503@gmail.com>
2024-11-15 17:59:33 -0300

    Páginas informações sobre aluno

---
+++ info_aluno.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Informações dos Alunos</title>
+    <link rel="stylesheet" href="info_aluno.css">
+</head>
+<body>
+
+    <div class="content">
+        <div class="title">Informações sobre os alunos</div>
+        <ul class="student-list">
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 1">
+                <span>Aluno 1</span>
+            </li>
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 2">
+                <span>Aluno 2</span>
+            </li>
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 3">
+                <span>Aluno 3</span>
+            </li>
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 4">
+                <span>Aluno 4</span>
+            </li>
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 5">
+                <span>Aluno 5</span>
+            </li>
+            <li class="student-item">
+                <img src="img.PNG" alt="Aluno 6">
+                <span>Aluno 6</span>
+            </li>
+        </ul>
+    </div>
+    
+    <footer class="footer">
+        <div class="footer-left">
+            <p>Copyright Arquitetos do Conhecimento 2024</p>
+        </div>
+        <div class="footer-right">
+            <h4>Acessibilidade</h4>
+            <div class="accessibility">
+                <span class="zoom-icon">🔍</span>
+                <span class="zoom-text">Aumentar Fonte</span>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="info_aluno.js"></script>
+</body>
+</html>

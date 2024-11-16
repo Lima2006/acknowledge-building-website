e5471e50286bf0855794efcb5df7113b4f256be7
Lima2006 <gabriellchagas.dev@gmail.com>
2024-11-16 00:48:38 -0300

    feat: protected route redirects user to login page

--- protect.php
+++ protect.php
@@ -3,6 +3,7 @@ if (!isset($_SESSION)) {
   session_start();
 }
 
-if (!isset($_SESSION["id"])) {
-  die ("<a href=\"login.php\">Entrar</a>");
+if (!isset($_SESSION['user']["id"])) {
+  header("Location: login.php");
+  die();
 }

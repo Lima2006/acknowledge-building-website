0ca2da9a4f0877b819700f5b14ede8c0eb293d54
AdnanCharif <adnancharif0@gmail.com>
2024-11-17 15:21:02 -0300

    estilizacao do criar turmas

--- criarturmas.php
+++ criarturmas.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Turma - Acknowledge Building</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
+    <link rel="stylesheet" href="criarturmas.css"> <!-- Link para o CSS -->
 </head>
 <body>
 <?php
@@ -13,37 +14,35 @@ include 'connection.php';
 $sql = "SELECT id, nome FROM assunto";
 $result = $conn->query($sql);
 ?>
-<form method="get" action="">
-    <div>
-        <div>
+<div class="form-container">
+    <form method="get" action="">
+        <div class="form-group">
             <label for="nome_da_turma">Nome da turma *</label>
+            <input type="text" name="nome_da_turma" class="input-field">
         </div>
-        <input type="text" name="nome_da_turma">
-    </div>
-    <div>
-        <div>
+        <div class="form-group">
             <label for="assuntos">Assunto *</label>
+            <select name="assunto" id="assunto" class="input-field">
+                <option value="0">Selecionar Assunto</option>
+                <?php
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
+                        }
+                    } else {
+                        echo "<option value=''>Nenhum nome encontrado</option>";
+                    }
+                ?>
+            </select>
         </div>
-        <select name="assunto" id="assunto">
-            <option value="0">Selecionar Assunto</option>
-            <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
-            }
-        } else {
-            echo "<option value=''>Nenhum nome encontrado</option>";
-        }
-        ?>
-        </select>
-    </div>
-    <div>
-    <div>
-    <label for="Descricao">Descrição</label>
-    </div>
-    <input type="e-mail" name="descricao">
-    </div>
-    <input type="submit" name="criar" value="Criar">    
-</form>
+        <div class="form-group">
+            <label for="Descricao">Descrição</label>
+            <textarea name="descricao" class="input-field"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="submit" name="criar" value="Criar" class="submit-button">
+        </div>
+    </form>
+</div>
 </body>
 </html>

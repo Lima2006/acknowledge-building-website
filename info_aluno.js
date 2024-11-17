9706e740cc1ad5ca28d61c5f94a7500dcaac476d
SPTulio <sergiotulio1503@gmail.com>
2024-11-17 19:31:04 -0300

    delete info_alunos

--- info_aluno.js
+++
@@ -1,5 +0,0 @@
-function aumentarFonte() {
-    const body = document.body;
-    let currentFontSize = parseFloat(window.getComputedStyle(body).fontSize);
-    body.style.fontSize = (currentFontSize + 2) + 'px';
-}
